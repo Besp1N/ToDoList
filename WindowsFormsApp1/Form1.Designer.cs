9f93c52018c8f47ad6f0341090fd7e318a2b7db4
Kacper Karabinowski <kackar87@wp.pl>
2023-03-30 22:20:34 +0200

    version 1.0

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel panel1;
+            System.Windows.Forms.Panel panel2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb1 = new System.Windows.Forms.Label();
+            this.tbToDo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete1 = new System.Windows.Forms.Button();
+            this.btnDelete2 = new System.Windows.Forms.Button();
+            this.btnDelete3 = new System.Windows.Forms.Button();
+            this.btnDelete4 = new System.Windows.Forms.Button();
+            panel1 = new System.Windows.Forms.Panel();
+            panel2 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.BackColor = System.Drawing.Color.Transparent;
+            this.lb1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.ForeColor = System.Drawing.Color.White;
+            this.lb1.Location = new System.Drawing.Point(116, 67);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(462, 38);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Welcome to your To Do App";
+            // 
+            // tbToDo
+            // 
+            this.tbToDo.Location = new System.Drawing.Point(399, 567);
+            this.tbToDo.Name = "tbToDo";
+            this.tbToDo.Size = new System.Drawing.Size(179, 20);
+            this.tbToDo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(118, 562);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What do you want to do?";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(451, 593);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(80, 20);
+            this.btnInput.TabIndex = 3;
+            this.btnInput.Text = "Wprowadz";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // panel1
+            // 
+            panel1.Location = new System.Drawing.Point(123, 532);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(462, 1);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.Location = new System.Drawing.Point(123, 161);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(462, 1);
+            panel2.TabIndex = 5;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl1.Location = new System.Drawing.Point(75, 20);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 24);
+            this.lbl1.TabIndex = 0;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl2.Location = new System.Drawing.Point(75, 91);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 24);
+            this.lbl2.TabIndex = 1;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl3.Location = new System.Drawing.Point(75, 159);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 24);
+            this.lbl3.TabIndex = 2;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl4.Location = new System.Drawing.Point(75, 228);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 24);
+            this.lbl4.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.btnDelete4);
+            this.panel3.Controls.Add(this.btnDelete3);
+            this.panel3.Controls.Add(this.btnDelete2);
+            this.panel3.Controls.Add(this.btnDelete1);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.lbl4);
+            this.panel3.Controls.Add(this.lbl3);
+            this.panel3.Controls.Add(this.lbl2);
+            this.panel3.Controls.Add(this.lbl1);
+            this.panel3.Location = new System.Drawing.Point(123, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(20);
+            this.panel3.Size = new System.Drawing.Size(462, 286);
+            this.panel3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(23, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(23, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(23, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(23, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "4";
+            // 
+            // btnDelete1
+            // 
+            this.btnDelete1.Location = new System.Drawing.Point(364, 20);
+            this.btnDelete1.Name = "btnDelete1";
+            this.btnDelete1.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete1.TabIndex = 8;
+            this.btnDelete1.Text = "Delete 1";
+            this.btnDelete1.UseVisualStyleBackColor = true;
+            this.btnDelete1.Click += new System.EventHandler(this.btnDelete1_Click);
+            // 
+            // btnDelete2
+            // 
+            this.btnDelete2.Location = new System.Drawing.Point(364, 91);
+            this.btnDelete2.Name = "btnDelete2";
+            this.btnDelete2.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete2.TabIndex = 9;
+            this.btnDelete2.Text = "Delete 2";
+            this.btnDelete2.UseVisualStyleBackColor = true;
+            this.btnDelete2.Click += new System.EventHandler(this.btnDelete2_Click);
+            // 
+            // btnDelete3
+            // 
+            this.btnDelete3.Location = new System.Drawing.Point(364, 159);
+            this.btnDelete3.Name = "btnDelete3";
+            this.btnDelete3.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete3.TabIndex = 10;
+            this.btnDelete3.Text = "Delete 3";
+            this.btnDelete3.UseVisualStyleBackColor = true;
+            this.btnDelete3.Click += new System.EventHandler(this.btnDelete3_Click);
+            // 
+            // btnDelete4
+            // 
+            this.btnDelete4.Location = new System.Drawing.Point(364, 228);
+            this.btnDelete4.Name = "btnDelete4";
+            this.btnDelete4.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete4.TabIndex = 11;
+            this.btnDelete4.Text = "Delete 4";
+            this.btnDelete4.UseVisualStyleBackColor = true;
+            this.btnDelete4.Click += new System.EventHandler(this.btnDelete4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(684, 651);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(panel2);
+            this.Controls.Add(panel1);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbToDo);
+            this.Controls.Add(this.lb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox tbToDo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete4;
+        private System.Windows.Forms.Button btnDelete3;
+        private System.Windows.Forms.Button btnDelete2;
+        private System.Windows.Forms.Button btnDelete1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,89 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        int counter = 0;
+        List<string> list = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void check()
+        {
+            if (counter == 4)
+            {
+                MessageBox.Show("Full items, delete old to add new", "Full items", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            if (lbl1.Text == string.Empty)
+            {
+                check();
+                counter++;
+                lbl1.Text = tbToDo.Text;
+                list.Add(lbl1.Text);
+                
+                tbToDo.Text = string.Empty;
+            }
+            else if (lbl2.Text == string.Empty)
+            {
+                check();
+                counter++;
+                lbl2.Text = tbToDo.Text; 
+                list.Add(lbl2.Text);
+                tbToDo.Text = string.Empty;
+
+            }
+            else if (lbl3.Text == string.Empty) 
+            {
+                check();
+                counter++;
+                lbl3.Text = tbToDo.Text;
+                list.Add (lbl3.Text);
+              
+                tbToDo.Text = string.Empty;
+
+            }
+            else
+            {
+                check();
+                counter++;
+                lbl4.Text = tbToDo.Text;
+                list.Add(lbl4.Text);
+              
+                tbToDo.Text = string.Empty;
+
+            }
+
+
+        }
+
+        private void btnDelete4_Click(object sender, EventArgs e)
+        {
+            counter--;
+            lbl4.Text = string.Empty;
+        }
+
+        private void btnDelete3_Click(object sender, EventArgs e)
+        {
+            counter--;
+            lbl3.Text = string.Empty;
+        }
+
+        private void btnDelete2_Click(object sender, EventArgs e)
+        {
+            counter--;
+            lbl2.Text = string.Empty;
+        }
+
+        private void btnDelete1_Click(object sender, EventArgs e)
+        {
+            counter--;
+            lbl1.Text = string.Empty;
+        }
     }
 }
