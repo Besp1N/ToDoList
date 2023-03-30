b8a83748c95d4ae5a6d9dc3a9360e031e58b548a
Kacper Karabinowski <kackar87@wp.pl>
2023-03-30 22:29:44 +0200

    version 1.2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.btnDelete2 = new System.Windows.Forms.Button();
             this.btnDelete3 = new System.Windows.Forms.Button();
             this.btnDelete4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             panel1 = new System.Windows.Forms.Panel();
             panel2 = new System.Windows.Forms.Panel();
             this.panel3.SuspendLayout();
@@ -176,9 +177,9 @@
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label2.Location = new System.Drawing.Point(23, 20);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(20, 24);
+            this.label2.Size = new System.Drawing.Size(25, 24);
             this.label2.TabIndex = 4;
-            this.label2.Text = "1";
+            this.label2.Text = "1.";
             // 
             // label3
             // 
@@ -187,9 +188,9 @@
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label3.Location = new System.Drawing.Point(23, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 24);
+            this.label3.Size = new System.Drawing.Size(25, 24);
             this.label3.TabIndex = 5;
-            this.label3.Text = "2";
+            this.label3.Text = "2.";
             // 
             // label4
             // 
@@ -198,9 +199,9 @@
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label4.Location = new System.Drawing.Point(23, 159);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.Size = new System.Drawing.Size(25, 24);
             this.label4.TabIndex = 6;
-            this.label4.Text = "3";
+            this.label4.Text = "3.";
             // 
             // label5
             // 
@@ -209,9 +210,9 @@
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label5.Location = new System.Drawing.Point(23, 228);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 24);
+            this.label5.Size = new System.Drawing.Size(25, 24);
             this.label5.TabIndex = 7;
-            this.label5.Text = "4";
+            this.label5.Text = "4.";
             // 
             // btnDelete1
             // 
@@ -253,12 +254,25 @@
             this.btnDelete4.UseVisualStyleBackColor = true;
             this.btnDelete4.Click += new System.EventHandler(this.btnDelete4_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(12, 629);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Created by Kacper Karabinowski";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(684, 651);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.panel3);
             this.Controls.Add(panel2);
             this.Controls.Add(panel1);
@@ -296,6 +310,7 @@
         private System.Windows.Forms.Button btnDelete3;
         private System.Windows.Forms.Button btnDelete2;
         private System.Windows.Forms.Button btnDelete1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
